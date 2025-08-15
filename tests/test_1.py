2d95c16f058a599374f214a73ce2a124e099af86
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-08-15 09:41:32 -0400

    Add/update test_1.py file

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,12 +1,12 @@
 import pytest
-from definition_e591f420d9d7415495e080a9f0478e11 import clean_text
+from definition_d36c34ec45844f2caa1890586e42b557 import clean_text
 
-@pytest.mark.parametrize("input_text, expected_output", [
+@pytest.mark.parametrize("input_text, expected_text", [
     ("  Hello World!  ", "hello world!"),
     ("This is a test.", "this is a test."),
-    ("Mixed Case Text", "mixed case text"),
-    ("Punctuation!@#$%^&*()_+=-`~[]\{}|;\':\",./<>?", "punctuation!@#$%^&*()_+=-`~[]\{}|;\':\",./<>?"),
-    ("Line\nBreaks\rAnd\tTabs", "line\nbreaks\rand\ttabs"),
+    ("Multiple   spaces   here.", "multiple spaces here."),
+    ("Punctuation!?", "punctuation!?"),
+    ("", ""),
 ])
-def test_clean_text(input_text, expected_output):
-    assert clean_text(input_text) == expected_output
+def test_clean_text(input_text, expected_text):
+    assert clean_text(input_text) == expected_text
